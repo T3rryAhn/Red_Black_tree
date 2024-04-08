f5a5e5b4139ec2db998fb089cbfcde353748198e
T3rryAhn <t3rryahn.dev@gmail.com>
2024-04-08 12:54:59 +0900

    feat: new_rbtree refactor nill node

--- src/rbtree.c
+++ src/rbtree.c
@@ -4,10 +4,18 @@
 
 rbtree *new_rbtree(void) {
     rbtree *p = (rbtree *) calloc(1, sizeof(rbtree));
-    // TODO: initialize struct if needed
-    p->nil = calloc(1, sizeof(node_t));
+
+    // 닐 노드 생성. 닐 노드를 가르키는 포인터 NILL 은 변경 불가능한 상수.
+    node_t * const NILL = calloc(1, sizeof(node_t));
+    NILL->color = RBTREE_BLACK;
+    NILL->key = -1;
+    NILL->left = NILL;
+    NILL->right = NILL;
+    NILL->parent = NILL;
+
+    p->nil = NILL;
     p->nil->color = RBTREE_BLACK;
-    p->root = p->nil;
+    p->root = NILL;
 
     return p;
 }
@@ -18,8 +26,13 @@ void delete_rbtree(rbtree *t) {
 }
 
 node_t *create_node(const key_t key) {
+    // TODO: 메모리에 노드 크기만큼 할당받고 값 채우고 주소 뱉어주기. 값만 가지고 트리와 연결되지않은 노드를 생성.
     node_t *temp_node = malloc(sizeof(node_t));
-
+    temp_node->color = RBTREE_RED;
+    temp_node->key = key;
+    temp_node->parent
+    temp_node->left = ;
+    temp_node->right
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
